072e9d7bab32f14fb531003146fa5e545bc6c873
David Leitner <david@leitner.io>
2019-04-25 05:03:06 +0200

    Initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  AfterContentChecked,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import { merge, of, fromEvent, Subscription, interval } from 'rxjs';
+import { switchMap, first } from 'rxjs/operators';
+
+const WARNING_AFTER = 10000;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>Inactivity Warning</h1>
+  `,
 })
-export class AppComponent {
-  title = 'ngTraining-zone';
+export class AppComponent implements OnInit, OnDestroy, AfterContentChecked {
+  private subscriptions: Subscription[] = [];
+
+  public ngOnInit(): void {
+    merge(
+      of(''),
+      fromEvent(document, 'click'),
+      fromEvent(document, 'mousemove'),
+      fromEvent(document, 'mousedown'),
+      fromEvent(document, 'touchstart'),
+      fromEvent(document, 'keypress'),
+      fromEvent(document, 'scroll')
+    )
+      .pipe(switchMap(() => interval(WARNING_AFTER).pipe(first())))
+      .subscribe(() => {
+        window.alert('you are inactive');
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
+
+  public ngAfterContentChecked(): void {
+    console.log('change');
+  }
 }
